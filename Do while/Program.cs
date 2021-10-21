cc142ba664b073633f387e4d7d757b1d3a9292c0
nvrsk <nvrsk@DESKTOP-1RBTMCR>
2021-10-21 19:06:47 +0300

    Добавьте файлы проекта.

---
+++ Do while/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, factor = 1;
+            Console.WriteLine("Введите положительное число, чтобы вычислить его факториал: ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            if (p <= 0)
+            {
+                Console.WriteLine("Вы гений? Введите ПОЛОЖИТЕЛЬНОЕ число:");
+                int c = Convert.ToInt32(Console.ReadLine());
+                {
+                    do
+                    {
+                        a += 1;
+                        factor *= a;
+
+                    } while (c != a);
+
+                    Console.WriteLine($"Факториал числа {c} равен: {factor}");
+                }
+            }
+            else if (p > 0)
+            {
+                do
+                {
+                    a += 1;
+                    factor *= a;
+
+                } while (p != a);
+
+                Console.WriteLine($"Факториал числа {p} равен: {factor}");
+            }
+
+ 
+        }
+    }
+}
